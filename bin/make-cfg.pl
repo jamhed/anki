c93d76349cc9384e21213e4d856f38a4bdb7b3ce
Roman Galeev <ip@ncom-ufa.ru>
2018-01-04 21:29:28 +0100

    Add config generator

---
+++ bin/make-cfg.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -w
+use JSON;
+use strict;
+
+my ($gn, $gp, $an, $ap) = @ARGV;
+unless ($ap) {
+    print "Usage: $0 google_login google_password anki_login anki_password\n";
+    exit;
+}
+
+my $cfg = {
+    google => {
+        login => $gn,
+        password => $gp
+    },
+    anki => {
+        login => $an,
+        password => $ap
+    }
+};
+
+print JSON->new->pretty(1)->encode( $cfg );
